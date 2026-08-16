80cb930750ee94868cf874bd282c460870643783
Charu-sarswat <charusaraswat639649@gmail.com>
2026-08-16 13:35:38 +0530

    add new image

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,7 +6,7 @@ const productsList = [
     id: 'ophthalmic',
     name: 'Ophthalmic Delivery System',
     desc: 'High-precision ophthalmic packaging for safe, sterile, & reliable drug delivery.',
-    img: '/https_/home prod/Ophthalmic Delivery System.webp',
+    img: '/orp.jpg',
     cover: true
   },
   {
